408c93ddcd64ed2c9ab6ca25f915c390eb84b894
Frank Congson <fcongson@gmail.com>
2023-05-08 11:55:16 +1200

    Iterate on typewriter heading

--- src/components/TypewriterHeading.vue
+++ src/components/TypewriterHeading.vue
@@ -17,15 +17,43 @@ export default {
 </script>
 
 <template>
-  <h1 v-if="level === '1'">{{ typed }}<span class="cursor">_</span></h1>
-  <h2 v-if="level === '2'">{{ typed }}<span class="cursor">_</span></h2>
-  <h3 v-if="level === '3'">{{ typed }}<span class="cursor">_</span></h3>
-  <h4 v-if="level === '4'">{{ typed }}<span class="cursor">_</span></h4>
-  <h5 v-if="level === '5'">{{ typed }}<span class="cursor">_</span></h5>
-  <h6 v-if="level === '6'">{{ typed }}<span class="cursor">_</span></h6>
+  <div class="typewriter">
+    <div class="hidden">
+      <h1 v-if="level === '1'">{{ title }}<span class="cursor">_</span></h1>
+      <h2 v-if="level === '2'">{{ title }}<span class="cursor">_</span></h2>
+      <h3 v-if="level === '3'">{{ title }}<span class="cursor">_</span></h3>
+      <h4 v-if="level === '4'">{{ title }}<span class="cursor">_</span></h4>
+      <h5 v-if="level === '5'">{{ title }}<span class="cursor">_</span></h5>
+      <h6 v-if="level === '6'">{{ title }}<span class="cursor">_</span></h6>
+    </div>
+    <div class="visible">
+      <h1 v-if="level === '1'">{{ typed }}<span class="cursor">_</span></h1>
+      <h2 v-if="level === '2'">{{ typed }}<span class="cursor">_</span></h2>
+      <h3 v-if="level === '3'">{{ typed }}<span class="cursor">_</span></h3>
+      <h4 v-if="level === '4'">{{ typed }}<span class="cursor">_</span></h4>
+      <h5 v-if="level === '5'">{{ typed }}<span class="cursor">_</span></h5>
+      <h6 v-if="level === '6'">{{ typed }}<span class="cursor">_</span></h6>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.typewriter {
+  display: grid;
+}
+.typewriter .hidden,
+.typewriter .visible {
+  grid-area: 1 / 1;
+  width: 100%;
+}
+.typewriter .hidden {
+  visibility: hidden;
+  z-index: 0;
+}
+.typewriter .visible {
+  z-index: 1;
+}
+
 h1,
 h2,
 h3,
@@ -44,5 +72,6 @@ h6 {
 }
 .cursor {
   animation: cursor-blink 1.5s steps(2) infinite;
+  display: inline-block;
 }
 </style>
