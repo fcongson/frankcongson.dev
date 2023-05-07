a076e2e0b268a01e944345bbacc500769ecbf719
Frank Congson <fcongson@gmail.com>
2023-05-08 11:26:11 +1200

    Iterate on typewriter heading

--- src/components/TypewriterHeading.vue
+++ src/components/TypewriterHeading.vue
@@ -1,16 +1,28 @@
 <script lang="ts">
 export default {
   props: ["title", "level"],
+  setup() {
+    let typed = "";
+    return {
+      typed,
+    };
+  },
+  mounted(this) {
+    window.setInterval(() => {
+      this.typed = this.$props.title.slice(0, this.typed.length + 1);
+      this.$forceUpdate();
+    }, 70);
+  },
 };
 </script>
 
 <template>
-  <h1 v-if="level === '1'">{{ title }}<span class="cursor">_</span></h1>
-  <h2 v-if="level === '2'">{{ title }}<span class="cursor">_</span></h2>
-  <h3 v-if="level === '3'">{{ title }}<span class="cursor">_</span></h3>
-  <h4 v-if="level === '4'">{{ title }}<span class="cursor">_</span></h4>
-  <h5 v-if="level === '5'">{{ title }}<span class="cursor">_</span></h5>
-  <h6 v-if="level === '6'">{{ title }}<span class="cursor">_</span></h6>
+  <h1 v-if="level === '1'">{{ typed }}<span class="cursor">_</span></h1>
+  <h2 v-if="level === '2'">{{ typed }}<span class="cursor">_</span></h2>
+  <h3 v-if="level === '3'">{{ typed }}<span class="cursor">_</span></h3>
+  <h4 v-if="level === '4'">{{ typed }}<span class="cursor">_</span></h4>
+  <h5 v-if="level === '5'">{{ typed }}<span class="cursor">_</span></h5>
+  <h6 v-if="level === '6'">{{ typed }}<span class="cursor">_</span></h6>
 </template>
 
 <style scoped>
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import work from "../../content/work.json";
+import TypewriterHeading from "./TypewriterHeading.vue";
 
 const formatDate = (date: string) =>
   new Date(date).toLocaleString("en-US", {
@@ -11,7 +12,8 @@ const formatDate = (date: string) =>
 <template>
   <section>
     <div class="container">
-      <h2>{{ work.title }}</h2>
+      <!-- <h2>{{ work.title }}</h2> -->
+      <TypewriterHeading v-bind:title="work.title" level="2" />
       <div v-for="experience in work.experiences" class="experience">
         <h3>
           {{ experience.jobTitle }} @
